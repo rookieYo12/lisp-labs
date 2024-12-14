f6683932c175d4c8dd91b0ef51cb7e679abf83f4
Andrey Kharitonov <a.kharitonovt.rex@gmail.com>
2024-12-14 18:49:25 +0300

    lab10: prog is working

--- lab10.lisp
+++ lab10.lisp
@@ -1,14 +1,5 @@
-(defun add_new_prev_specified (List)
-  (format t "Input element: ")
-  (_add_new_prev_specified List (read-char)))
-
-(defun _add_new_prev_specified (List Elem)
+(defun f (List Elem)
   (cond
     ((null List) nil)
-    ((eql Elem (car List)) (rplaca (rplacd List List) Elem))
-    (t (_add_new_prev_specified (cdr List) Elem))))
-
-(defun f (List)
-  (cond
-    ((null List) nil)
-    (t (f (cddr (rplaca (rplacd List List) 'n))))))
+    ((eql Elem (car List)) (f (cddr (rplaca (rplacd List (cons (car List) (cdr List))) 'n)) Elem))
+    (t (f (cdr List) Elem))))
